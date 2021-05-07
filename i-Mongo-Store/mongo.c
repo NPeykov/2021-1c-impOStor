5a13771241273e51c3b13ed81721d7db6dba3d7b
gabiquintana98 <gabrielqtemoche@hotmail.com>
2021-05-07 16:34:49 -0300

    modulo i mongo store

---
+++ i-Mongo-Store/mongo.c
@@ -0,0 +1,18 @@
+#include "mongo.h"
+
+
+int main(int t, char * arg[]){
+
+//	mongoConfig = config_create("mongo.config");
+//	ip = config_get_string_value(sindicatoConfig, "IP");
+//	puerto = config_get_string_value(sindicatoConfig, "PUERTO");
+//
+//	logger = log_create("mongo.log", "Mongo", 1, LOG_LEVEL_DEBUG);
+//
+//	servidorMongo = iniciar_servidor(ip, puerto);
+//
+//	crearEstructuraFileSystem();
+//
+//	return 0;
+
+}
---
+++ i-Mongo-Store/mongo.h
@@ -0,0 +1,32 @@
+/*
+ * mongo.h
+ *
+ *  Created on: 7 may. 2021
+ *      Author: utnso
+ */
+
+#ifndef I_MONGO_STORE_MONGO_H_
+#define I_MONGO_STORE_MONGO_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <utils/utils.h>
+#include <commons/config.h>
+#include <commons/bitarray.h>
+#include <commons/collections/list.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <commons/string.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <readline/readline.h>
+
+char* ARCHIVO_CONFIG = "mongo.config";
+#endif /* I_MONGO_STORE_MONGO_H_ */
+#ifndef SINDICATO_H_
+#define SINDICATO_H_
+

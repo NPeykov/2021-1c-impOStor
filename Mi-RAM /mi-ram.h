7a576f4cabc27591e5afac0a0b82fa9a450861a8
Mauro <maurorossi68@hotmail.com>
2021-05-27 14:30:19 -0300

    Agregadas estructuras de PCB y TCB

--- Mi-RAM /mi-ram.h
+++ Mi-RAM /mi-ram.h
@@ -17,4 +17,21 @@ t_config *config;
 char* puerto;
 int socket_cliente;
 
+// Todos los int de 32bits hacen referencia a una direccion en la memoria
+
+typedef struct{
+	uint32_t pid; //Identificador de la Patota
+	uint32_t tareas; //Lista de tareas de la patota
+} pcb; //Patota Control Block
+
+typedef struct{
+	uint32_t tid;//Identificador del Tripulante
+	char status;//Estado del tripulante (Ejecucion,Bloqueado,Nuevo o Listo)
+	uint32_t posX;//Posicion en el eje x
+	uint32_t posY;//Posicion en el eje Y
+	uint32_t proxIns; //Proxima Instruccion
+	uint32_t pcb; //Direccion de la PCB asociada al tripulante
+} tcb; //Tripulante Control Block
+
+
 #endif

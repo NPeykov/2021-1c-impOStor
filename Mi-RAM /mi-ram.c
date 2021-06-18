73b345fd0d5dc8883136b8fd1c3b271cb2df52c9
Francisco Blasco <francisco.o.blasco@gmail.com>
2021-06-18 18:22:26 -0300

    Segmentacion casi lista creacion de segmentos

--- Mi-RAM /mi-ram.c
+++ Mi-RAM /mi-ram.c
@@ -1,5 +1,34 @@
 #include "mi-ram.h"
 
+Segmento* crear_segmento(char *tareasNuevas[], tipo_segmento tipo, int tamanio, t_list* tabla_segmentos){
+	Segmento *segmento;
+
+	switch(tipo){
+	case(tipo == PCB):
+		segmento=crear_segmento_pcb(numero_patota, tamanio, tabla_segmentos);
+		numero_patota += 1;
+		break;
+	case(tipo == TCB):
+		segmento=crear_segmento_tcb(tamanio, tabla_segmentos);
+		break;
+	case(tipo == TAREAS):
+		segmento=crear_segmento_tareas(tamanio, tabla_segmentos);
+		break;
+	default:
+		printf("Este es un tipo de segmento no válido");
+	}
+
+	return segmento;
+}
+
+Segmento* crear_segmento_pcb(int numero_patota,int tamanio, tabla_segmentos){
+	PatotaCB pcb = (PatotaCB*) malloc(sizeof(PatotaCB));
+	pcb->pid = numero_patota;
+	sem_wait(&tareas_creadas);
+	pcb->tareas = get_segmento_tareas_patota(numero_patota);
+	sem_post(&tareas_creadas);
+}
+
 void *gestionarCliente(int socket) {
 		int conexionCliente;
 		t_list* lista;
@@ -16,6 +45,10 @@ void *gestionarCliente(int socket) {
 			printf("\nLA OPERACION ES: %d\n", operacion);
 
 			switch(operacion) {
+				case INICIAR_PATOTA:
+					lista = recibir_paquete(cliente);
+					t_list* tabla_segmentos = list_create(); //Tabla de Patota
+					break;
 				case EXPULSAR_TRIPULANTE:
 					lista = recibir_paquete(cliente);
 					printf("Tripulante eliminado de la nave %s\n", (char *) list_get(lista,0));
@@ -34,27 +67,6 @@ void *gestionarCliente(int socket) {
 		}
 	}
 
-void crearRAM(t_config *config, struct nodoMemoria *memoria){
-
-	char* tipoMemoria = config_get_string_value(config, "ESQUEMA_MEMORIA");
-	printf("Paso 2");
-	/*
-	//Segun que esquema de memoria se arma la memoria a eso
-	switch(atoi(tipoMemoria)){
-	case 0:
-		printf("Se intentó paginación");
-		//TODO: PAGINACION
-		break;
-	case 1:
-		printf("Se intento segmentación");
-		//TODO: SEGMENTACION
-		break;
-	default:
-		printf("Error, esquema de memoria desconocido.\n");
-		break;
-	}*/
-}
-
 void inicializar_ram(){
 	printf("################# Modulo Mi-RAM #################\n");
 	//logger = log_create(archivoDeLog, "CoMAnda", 1, LOG_LEVEL_DEBUG);
@@ -66,55 +78,32 @@ void inicializar_ram(){
 	puerto = config_get_string_value(config, "PUERTO");
 
 	printf("MI_RAM escuchando en PUERTO:%s \n", puerto);
-	printf("Pasa 1");
-	crearRAM(config, memoria);
-	printf("Pasa 3");
+	char* tipoMemoria = config_get_string_value(config, "ESQUEMA_MEMORIA");
+	switch(atoi(tipoMemoria)){
+		case 0:
+			printf("Se intentó paginación");
+			//TODO: PAGINACION
+			break;
+		case 1:
+			printf("Se intento segmentación");
+
+			break;
+		default:
+			printf("Error, esquema de memoria desconocido.\n");
+			break;
+	}
 	gestionarCliente(socket_mi_ram);
 	//memoriaPrincipal = malloc(tamanioMemoria);
 	//memoriaSwap = malloc(tamanioSwap);
-	//restaurantes = list_create();
 
 }
-
 
 
 
 int main(){
   inicializar_ram();
 
-
-  /*
-  int socket_mi_ram;
-  t_config *config;
-  char* puerto;
-  int socket_cliente;
-
-  socket_mi_ram = levantar_servidor(MI_RAM_HQ);
-
-  //-------------------------------------------------------//
-
-  config = config_create(PATH_MI_RAM_CONFIG); //aca estarian todas las configs de este server
-
-  puerto = config_get_string_value(config, "PUERTO");
-
-  printf("MI_RAM escuchando en PUERTO:%s \n", puerto);
-
-  socket_cliente = esperar_cliente(socket_mi_ram);
-
-
-  while(1) {
-	int cliente=-1;
-	cliente = esperar_cliente(servidor);
-	if(cliente!=-1) {
-	   gestionarCliente(cliente);
-	   //pthread_create(&thread0, NULL, gestionarCliente, cliente);
-	   //pthread_detach(thread0);
-	}
-  }
-
-  //pthread_join(thread0, NULL);
-*/
-
+  sem_init(&tareas_creadas, 0, 0);
 
   return EXIT_SUCCESS;
 }
--- Mi-RAM /mi-ram.h
+++ Mi-RAM /mi-ram.h
@@ -9,26 +9,31 @@
 #include "../utils/utils.h"
 
 int servidor;
-
 int socket_mi_ram;
 t_config *config;
 char* puerto;
 int socket_cliente;
-char *tareas[];
 t_list* patotas; //Lista de tablas de segmentos
+int numero_patota = 1;
 
 // Todos los int de 32bits hacen referencia a una direccion en la memoria
+typedef enum tipo_segmento {
+	PCB, TCB, TAREAS
+} tipo_segmento;
+
 
 typedef struct{
 	int idSegmento;
-	uint32_t inicio;
+	tipo_segmento tipo;
+	uint32_t base;
 	int tamanio;
-} TablaSegmentos; //Patota
+	void *dato;
+} Segmento; //Patota
 
 typedef struct{
 	uint32_t pid; //Identificador de la Patota
 	uint32_t tareas; //Direccion Lista de tareas de la patota
-} pcb; //Patota Control Block
+} PatotaCB; //Patota Control Block
 
 typedef struct{
 	uint32_t tid;//Identificador del Tripulante
@@ -37,27 +42,11 @@ typedef struct{
 	uint32_t posY;//Posicion en el eje Y
 	uint32_t proxIns; //Proxima Instruccion
 	uint32_t pcb; //Direccion de la PCB asociada al tripulante
-} tcb; //Tripulante Control Block
-
-
-// La memoria se estructurara a modo de listas enlazadas
-// Cada nodo de la lista es una posicion de memoria que almacena un dato
-// Este dato es un dato de 32bits
-// Y esta posicion de memoria conoce a la siguiente posicion
-
-typedef struct{
-	uint32_t dato;
-	struct nodoMemoria *siguiente;
-}nodoMemoria;
+} TripuCB; //Tripulante Control Block
 
 typedef enum{
 	PAGINACION, SEGMENTACION
 }esquemaMemoria;
-
-struct nodoMemoria *memoria = NULL;
-TablaSegmentos* nuevaPatota;
-
-void crearRAM(t_config* config, struct nodoMemoria *memoria);
 
 void *gestionarCliente(int cliente);
 

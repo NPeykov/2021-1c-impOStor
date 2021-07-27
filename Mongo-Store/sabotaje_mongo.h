ae3d9f39c67ae5c344cc5ae709c562cb3cf4bc53
peyk0v-vm <peykovn@gmail.com>
2021-07-27 16:31:02 -0300

    agrego bajadas a disco y planteo sabotaje

---
+++ Mongo-Store/sabotaje_mongo.h
@@ -0,0 +1,23 @@
+#ifndef SABOTAJE_MONGO_H
+#define SABOTAJE_MONGO_H
+
+#include "../utils/utils.h"
+#include "mongo-store.h"
+
+typedef enum{
+	SB_BLOCKS,
+	SB_BITMAP,
+	FILES_SIZE,
+	FILES_BLOCK_COUNT,
+	FILES_MD5,
+	NO_HAY_SABOTAJE
+}sabotaje_code;
+
+void rutina(int);
+sabotaje_code obtener_tipo_sabotaje();
+void gestionarSabotaje(void);
+char* siguiente_posicion_sabotaje(void);
+void enviar_aviso_sabotaje_a_discordiador(void *);
+
+
+#endif
